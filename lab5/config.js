9464b1ee62299eddd6c1125cf20dbae7af844a07
ruslana-novikova22 <128227563+ruslana-novikova22@users.noreply.github.com>
2024-04-04 00:36:25 +0300

    Delete lab5 directory

--- lab5/config.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-
-const config = {
-    port: process.env.PORT || 7000,
-    mongodb_uri: process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/lab2',
-};
-
-module.exports = config;
--- lab5/enums/bookFirm.enum.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    Book_Emporium : "Book Emporium",
-    Literary_Haven : "Literary Haven"
-};
